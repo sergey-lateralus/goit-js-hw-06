fa8c2dfbdcde3392716c8cbc74a592e316607b51
sergey-lateralus <sergey.nezhyn@gmail.com>
2021-11-10 00:59:27 +0200

    Добавляет решение задачи 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeButtonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+changeButtonEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  spanEl.textContent = randomColor;
+  bodyEl.style.backgroundColor = randomColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
